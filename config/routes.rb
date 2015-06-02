4c2944c3cc33d49780520dd2b60ac81adb0682d6
cgdibble <cgdibble@gmail.com>
2015-06-01 22:53:33 -0700

    added resources and models

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  resource :blogs do
+    resources :comments
+  end
+
   root 'blog#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
---
+++ db/migrate/20150602055304_create_comments.rb
@@ -0,0 +1,12 @@
+class CreateComments < ActiveRecord::Migration
+  def change
+    create_table :comments do |t|
+      t.string :user_name
+      t.string :title
+      t.text :conent
+      t.integer :blog_id
+
+      t.timestamps
+    end
+  end
+end
