a37b63561a818cb9814a9a7033c04df35aeb86bb
cgdibble <cgdibble@gmail.com>
2015-06-11 21:02:06 -0700

    removed faulty dependency angular stuff so master runs server now

--- app/assets/javascripts/front_end.js
+++ app/assets/javascripts/front_end.js
@@ -1,7 +1,7 @@
 (function() {
   var app = angular.module('twistthetides', []);
 
-  app.controller("BlogController", ["$scope", "$http", "$routeParams", "$location","ngDialog"], function() {
+  app.controller("BlogController", function() {
   	// ping Rails endpoint to get blogs and display them approprietly
   		// have a button/spot they can click that will open up the comments on the blog post if there are any.
   });
@@ -13,7 +13,7 @@
 		// - date + time posted
 		// - link to post/content in the directory
 		// - [array, of, comments]
-				// - Name(Default 'Anonymous')	
+				// - Name(Default 'Anonymous')
 				// - title ??? (currently in the migration)
 				// - Body
 				// - date + time posted
